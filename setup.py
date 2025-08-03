625b737a90313e23ae666094a67b1d62638117f8
a.nadafian <a.nadafian@binaexperts.com>
2025-08-03 17:26:02 +0330

    Initial commit: Add Cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An NLP project to classify news articles as real or fake using PyTorch and Hugging Face Transformers.',
+    author='Ashkan Nadafian',
+    license='MIT',
+)
